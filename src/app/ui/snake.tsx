216f5e7c6e8d665eebb95b38c013856feeb0f033
Kacper Skowroński <kacpermariaskowronski@protonmail.com>
2023-12-14 14:30:51 +0100

    Added food, and keeping score

--- src/app/ui/snake.tsx
+++ src/app/ui/snake.tsx
@@ -1,79 +1,111 @@
 "use client";
-import { useEffect, useState, useRef } from "react";
+import { useEffect, useState, useRef, useMemo } from "react";
 
 export default function Snake() {
   const canvasRef = useRef<HTMLCanvasElement>(null);
   const [ctx, setCtx] = useState<CanvasRenderingContext2D | null>(null);
+  const [score, setScore] = useState<number>(0);
+  const [play, setPlay] = useState<boolean>(false);
+  const fps: number = 30;
 
-  const head = {
-    x: 275,
-    y: 275,
-    radius: 25,
-    color: "white",
-    vx: 0,
-    vy: 0,
-  };
+  const head = useMemo(
+    () => ({
+      x: 280,
+      y: 280,
+      radius: 20,
+      color: "white",
+      vx: 0,
+      vy: 0,
+    }),
+    []
+  );
+
+  const food = useMemo(
+    () => ({
+      x: roundNearest5(Math.random() * 600),
+      y: roundNearest5(Math.random() * 600),
+      radius: 20,
+      color: "red",
+    }),
+    []
+  );
+
+  function roundNearest5(num: number) {
+    return Math.round(num / 20) * 20;
+  }
+
+  function start(event: React.KeyboardEvent) {
+    if (event.key === "ArrowUp") {
+      head.vy = -20;
+      head.vx = 0;
+    }
+    if (event.key === "ArrowDown") {
+      head.vy = 20;
+      head.vx = 0;
+    }
+    if (event.key === "ArrowLeft") {
+      head.vx = -20;
+      head.vy = 0;
+    }
+    if (event.key === "ArrowRight") {
+      head.vx = 20;
+      head.vy = 0;
+    }
+    if (event.code === "Space") {
+      head.vx = 0;
+      head.vy = 0;
+    }
+  }
+  const canvas = document.querySelector("canvas");
 
   useEffect(() => {
+    canvas?.focus();
     if (canvasRef.current) setCtx(canvasRef.current.getContext("2d"));
-  }, [canvasRef]);
 
-  useEffect(() => {
+    canvas?.addEventListener("keydown", draw);
+
     if (ctx) {
       ctx.fillStyle = head.color;
       ctx.fillRect(head.x, head.y, head.radius, head.radius);
     }
-  }, [ctx, head.color, head.radius, head.x, head.y]);
-
-  function start(event: React.KeyboardEvent) {
-    if (event.key === "ArrowUp") {
-      head.vy = -5;
-      head.vx = 0;
+    function draw() {
+      setPlay(true);
+      canvas?.removeEventListener("keydown", draw);
+      if (ctx) {
+        ctx.clearRect(0, 0, 600, 600);
+        ctx.fillStyle = head.color;
+        if (head.x + head.vx > 580 || head.x + head.vx < 0) head.vx = -head.vx;
+        if (head.y + head.vy > 580 || head.y + head.vy < 0) head.vy = -head.vy;
+        ctx.fillRect(head.x, head.y, head.radius, head.radius);
+        head.x += head.vx;
+        head.y += head.vy;
+        ctx.fillStyle = food.color;
+        ctx.fillRect(food.x, food.y, food.radius, food.radius);
+        if (head.x === food.x && head.y === food.y) {
+          food.x = roundNearest5(Math.random() * 600);
+          food.y = roundNearest5(Math.random() * 600);
+          setScore((score) => score + 1);
+        }
+        if (play) {
+          return;
+        }
+        setTimeout(() => {
+          requestAnimationFrame(draw);
+        }, 1000 / fps);
+      }
     }
-    if (event.key === "ArrowDown") {
-      head.vy = 5;
-      head.vx = 0;
-    }
-    if (event.key === "ArrowLeft") {
-      head.vx = -5;
-      head.vy = 0;
-    }
-    if (event.key === "ArrowRight") {
-      head.vx = 5;
-      head.vy = 0;
-    }
-    if (event.key === "Space") {
-      head.vx = 0;
-      head.vy = 0;
-    }
-    draw();
-  }
-
-  function draw() {
-    if (ctx) {
-      ctx.clearRect(0, 0, 600, 600);
-      ctx.fillStyle = head.color;
-      if (head.x + head.vx > 600 || head.x + head.vx < 0) head.vx = -head.vx;
-      if (head.y + head.vy > 600 || head.y + head.vy < 0) head.vy = -head.vy;
-      ctx.fillRect(
-        head.x + head.vx,
-        head.y + head.vy,
-        head.radius,
-        head.radius
-      );
-      head.x += head.vx;
-      head.y += head.vy;
-      requestAnimationFrame(draw);
-    }
-  }
+  }, [ctx, fps, score, head, food, play, canvas]);
 
   return (
-    <canvas
-      tabIndex={0}
-      ref={canvasRef}
-      width="600"
-      height="600"
-      onKeyDown={start}
-    />
+    <div>
+      <p>Score: {score}</p>
+      <canvas
+        tabIndex={0}
+        ref={canvasRef}
+        width="600"
+        height="600"
+        onKeyDown={start}
+      />
+    </div>
   );
 }
