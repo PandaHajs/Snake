5b110730fee7d85b307477d60ffba4891fb83c3c
Kacper Skowroński <kacpermariaskowronski@protonmail.com>
2023-12-14 22:13:39 +0100

    Added snakes tail (mostly working)

--- src/app/ui/snake.tsx
+++ src/app/ui/snake.tsx
@@ -7,6 +7,7 @@ export default function Snake() {
   const [score, setScore] = useState<number>(0);
   const [play, setPlay] = useState<boolean>(false);
   const fps: number = 15;
+  let tail: { x: number; y: number }[] = [];
 
   const head = useMemo(
     () => ({
@@ -51,40 +52,59 @@ export default function Snake() {
       head.vx = 50;
       head.vy = 0;
     }
-    if (event.code === "Space") {
-      head.vx = 0;
-      head.vy = 0;
-    }
+  }
+  if (ctx) {
+    ctx.fillStyle = head.color;
+    ctx.fillRect(head.x, head.y, head.radius, head.radius);
   }
   const canvas = document.querySelector("canvas");
-
   useEffect(() => {
     canvas?.focus();
     if (canvasRef.current) setCtx(canvasRef.current.getContext("2d"));
 
     canvas?.addEventListener("keydown", draw);
 
-    if (ctx) {
-      ctx.fillStyle = head.color;
-      ctx.fillRect(head.x, head.y, head.radius, head.radius);
-    }
     function draw() {
       setPlay(true);
       canvas?.removeEventListener("keydown", draw);
       if (ctx) {
         ctx.clearRect(0, 0, 600, 600);
         ctx.fillStyle = head.color;
+
+        let nextPositions = [{ x: head.x, y: head.y }, ...tail];
+
+        nextPositions.pop();
+
         if (head.x + head.vx > 550 || head.x + head.vx < 0) head.vx = -head.vx;
-        if (head.y + head.vy > 550 || head.y + head.vy < 0) head.vy = -head.vy;
-        ctx.fillRect(head.x, head.y, head.radius, head.radius);
         head.x += head.vx;
+        if (head.y + head.vy > 550 || head.y + head.vy < 0) head.vy = -head.vy;
         head.y += head.vy;
+        ctx.fillRect(head.x, head.y, head.radius, head.radius);
+
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+        tail = nextPositions;
+        tail.forEach((part) => {
+          ctx.fillStyle = "gray";
+          ctx.fillRect(part.x, part.y, head.radius, head.radius);
+
+          if (head.x === part.x && head.y === part.y) {
+            setScore(0);
+            head.x = 250;
+            head.y = 250;
+            head.vx = 0;
+            head.vy = 0;
+            tail = [];
+            return alert("Game Over");
+          }
+        });
+
         ctx.fillStyle = food.color;
         ctx.fillRect(food.x, food.y, food.radius, food.radius);
         if (head.x === food.x && head.y === food.y) {
           food.x = roundNearest50(Math.random() * 550);
           food.y = roundNearest50(Math.random() * 550);
           setScore((score) => score + 1);
+          nextPositions.unshift({ x: head.x, y: head.y });
         }
         if (play) {
           return;
@@ -94,7 +114,7 @@ export default function Snake() {
         }, 1000 / fps);
       }
     }
-  }, [ctx, fps, score, head, food, play, canvas]);
+  }, [ctx, fps, score, head, food, play, canvas, tail]);
 
   return (
     <div>
